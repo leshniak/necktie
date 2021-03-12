937408f1b309466b47a32a60a86268ae8d5ac594
Maciej Leśniewski <leshniack@gmail.com>
2021-03-12 08:25:03 +0100

    Adds MutationObserverMock

--- test/Necktie.spec.ts
+++ test/Necktie.spec.ts
@@ -7,6 +7,33 @@ const getInstance = (doc: JSDOM) =>
   new Necktie((doc.window as unknown) as Window & typeof globalThis, doc.window.document);
 
 describe('Necktie', () => {
+  let mutationObserverContexts: Array<MutationObserverMock>;
+
+  class MutationObserverMock implements MutationObserver {
+    public readonly observer: MutationCallback;
+
+    constructor(observer: MutationCallback) {
+      this.observer = observer;
+      mutationObserverContexts.push(this);
+    }
+
+    observe = jest.fn<void, [Node, MutationObserverInit]>();
+    disconnect = jest.fn<void, []>();
+    takeRecords = jest.fn<Array<MutationRecord>, []>();
+  }
+
+  beforeAll(() => {
+    global.MutationObserver = MutationObserverMock;
+  });
+
+  beforeEach(() => {
+    mutationObserverContexts = [];
+  });
+
+  afterEach(() => {
+    mutationObserverContexts = [];
+  });
+
   it('should create an instance', () => {
     const doc = emptyDocument();
 
